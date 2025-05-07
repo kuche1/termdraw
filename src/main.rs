741b6a6bd1575de8c5b7d22f79c9948e497440c6
kuche1 <znamsiparolata4esno@abv.bg>
2025-05-07 19:32:57 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use crossterm::{
+    // cargo add crossterm
+    ExecutableCommand,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+};
+use std::io::stdout;
+
+const PIXEL: &str = "▀";
+
+type Col = (u8, u8, u8);
+
+fn print_pixel(color_top: Col, color_bot: Col) {
+    let (tr, tg, tb) = color_top;
+    let (br, bg, bb) = color_bot;
+
+    let mut stdout = stdout();
+
+    stdout
+        .execute(SetForegroundColor(Color::Rgb {
+            r: tr,
+            g: tg,
+            b: tb,
+        }))
+        .unwrap();
+
+    stdout
+        .execute(SetBackgroundColor(Color::Rgb {
+            r: br,
+            g: bg,
+            b: bb,
+        }))
+        .unwrap();
+
+    stdout.execute(Print(PIXEL)).unwrap();
+
+    stdout.execute(ResetColor).unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_pixel((255, 0, 0), (0, 255, 0));
+    println!();
 }
